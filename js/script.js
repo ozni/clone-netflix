11db4ef98a47738f15e094e473d5fd80e6e11346
ozni <46676108+ozni@users.noreply.github.com>
2021-01-03 21:32:30 -0300

    fac implementado

---
+++ js/script.js
@@ -0,0 +1,54 @@
+span1 = document.querySelector('#what')
+info1 = document.querySelector('#inf1')
+
+span1.addEventListener('click', ()=>{
+    if (info1.style.display != "block"){
+        info1.style.display = "block"
+    } else {
+        info1.style.display = "none"
+    }
+})
+
+span2 = document.querySelector('#how')
+info2 = document.querySelector('#inf2')
+
+span2.addEventListener('click', ()=>{
+    if (info2.style.display != "block"){
+        info2.style.display = "block"
+    } else {
+        info2.style.display = "none"
+    }
+})
+
+span3 = document.querySelector('#when')
+info3 = document.querySelector('#inf3')
+
+span3.addEventListener('click', ()=>{
+    if (info3.style.display != "block"){
+        info3.style.display = "block"
+    } else {
+        info3.style.display = "none"
+    }
+})
+
+span4 = document.querySelector('#cancel')
+info4 = document.querySelector('#inf4')
+
+span4.addEventListener('click', ()=>{
+    if (info4.style.display != "block"){
+        info4.style.display = "block"
+    } else {
+        info4.style.display = "none"
+    }
+})
+
+span5 = document.querySelector('#what2')
+info5 = document.querySelector('#inf5')
+
+span5.addEventListener('click', ()=>{
+    if (info5.style.display != "block"){
+        info5.style.display = "block"
+    } else {
+        info5.style.display = "none"
+    }
+})
